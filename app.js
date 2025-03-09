3e4742596ed4b1652e54792908fec0f35ae417ae
Harshad Vilas Magadum <hvmagadum04@gmail.com>
2025-03-09 21:30:52 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config()
+const app = express();
+const port = 5012;
+
+app.get('/', (req,res)=>{
+    res.send("server is ready");
+});
+
+app.get('/About', (req,res)=>{
+    res.send("<h1>This is about page</h1>");
+})
+
+app.listen(port, () => {
+    console.log('Server is running on http://localhost:port');
+});
