4bf52af7f5633e40eaf81651e532c7dddf83582b
Harshad Vilas Magadum <hvmagadum04@gmail.com>
2025-03-09 21:37:53 +0530

    Added Logout Route

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/login', (req, res) => {
     res.send("<h1>Login Page</h1>");
 })
 
+app.get('/logout', (req, res) => {
+    res.send("<h1>This is Logout Page</h1>")
+})
+
 app.listen(port, () => {
     console.log('Server is running on http://localhost:port');
 });
